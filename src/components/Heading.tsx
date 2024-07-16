876f72f269344750888fa97bbad999d16833f33e
yusrilprayoga-code <124210006@student.upnyk.ac.id>
2024-07-16 13:56:20 +0700

    Update Layout

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -22,7 +22,7 @@ export const Heading = ({
     <Tag
       className={twMerge(
         CalSans.className,
-        "text-base md:text-xl lg:text-4xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary dark:text-gray-300",
+        "text-base md:text-xl lg:text-4xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary",
         className
       )}
     >
