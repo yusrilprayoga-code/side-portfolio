3e7a0114e74e115b5c85d781326dd0b597ff9afa
yusrilprayoga-code <124210006@student.upnyk.ac.id>
2024-07-16 18:22:35 +0700

    Update Products

--- src/constants/products.tsx
+++ src/constants/products.tsx
@@ -33,17 +33,17 @@ export const products = [
     ),
   },
   {
-    href: "https://algochurn.com",
-    title: "Algochurn",
+    href: "https://car-showroom-coral.vercel.app/",
+    title: "CarShowroom",
     description:
-      "Practice for technical interviews with hands on coding challenges.",
-    thumbnail: "/images/sidefolio-algochurn.png",
+      "A design and development studio that focuses on building quality apps.",
+    thumbnail: "/images/sidefolio-carshowroom.png",
     images: [
-      "/images/sidefolio-algochurn.png",
-      "/images/sidefolio-algochurn-2.png",
+      "/images/sidefolio-carshowroom.png",
+      "/images/sidefolio-carshowroom-2.png",
     ],
-    stack: ["Nextjs", "Tailwindcss"],
-    slug: "algochurn",
+    stack: ["Nextjs", "Tailwindcss", "Axios"],
+    slug: "carshowroom",
     content: (
       <div>
         <p>
@@ -66,17 +66,50 @@ export const products = [
     ),
   },
   {
-    href: "https://gomoonbeam.com",
-    title: "Moonbeam",
+    href: "https://nike-landing-page-chi.vercel.app/",
+    title: "Nike",
     description:
-      "Never write from scratch again with Moonbeam, your AI first writing tool",
-    thumbnail: "/images/sidefolio-moonbeam.png",
+      "Nike is a design and development studio that focuses on building quality apps.",
+    thumbnail: "/images/sidefolio-nike.png",
     images: [
-      "/images/sidefolio-moonbeam.png",
-      "/images/sidefolio-moonbeam-2.png",
+      "/images/sidefolio-nike.png",
+      "/images/sidefolio-nike-2.png",
     ],
-    stack: ["Nextjs", "Tailwindcss"],
-    slug: "moonbeam",
+    stack: ["Reactjs", "Tailwindcss"],
+    slug: "nike",
+    content: (
+      <div>
+        <p>
+          Sit eiusmod ex mollit sit quis ad deserunt. Sint aliqua aliqua ullamco
+          dolore nulla amet tempor sunt est ipsum. Dolor laborum eiusmod
+          cupidatat consectetur velit ipsum. Deserunt nisi in culpa laboris
+          cupidatat elit velit aute mollit nisi. Officia ad exercitation laboris
+          non cupidatat duis esse velit ut culpa et.{" "}
+        </p>
+        <p>
+          Exercitation pariatur enim occaecat adipisicing nostrud adipisicing
+          Lorem tempor ullamco exercitation quis et dolor sint. Adipisicing sunt
+          sit aute fugiat incididunt nostrud consequat proident fugiat id.
+          Officia aliquip laborum labore eu culpa dolor reprehenderit eu ex enim
+          reprehenderit. Cillum Lorem veniam eu magna exercitation.
+          Reprehenderit adipisicing minim et officia enim et veniam Lorem
+          excepteur velit adipisicing et Lorem magna.
+        </p>{" "}
+      </div>
+    ),
+  },
+  {
+    href: "https://nike-landing-page-chi.vercel.app/",
+    title: "ArCipe",
+    description:
+      "ArCipe is a design and development studio that focuses on building quality apps.",
+    thumbnail: "/images/sidefolio-arcipe.png",
+    images: [
+      "/images/sidefolio-arcipe.png",
+      "/images/sidefolio-arcipe-2.png",
+    ],
+    stack: ["Reactjs", "Tailwindcss"],
+    slug: "arcipe",
     content: (
       <div>
         <p>
