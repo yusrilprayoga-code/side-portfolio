638395ae6c2a393ae59c93e71110ac889dd8d165
yusrilprayoga-code <124210006@student.upnyk.ac.id>
2024-07-16 19:27:23 +0700

    Update Accomplishments Layouts

--- src/components/AccomplismentsLayout.tsx
+++ src/components/AccomplismentsLayout.tsx
@@ -187,7 +187,7 @@ const SkeletonFour = () => {
         className="h-full w-1/3 rounded-2xl bg-white p-4 dark:bg-black dark:border-white/[0.1] border border-neutral-200 flex flex-col items-center justify-center"
       >
         <Image
-          src="https://pbs.twimg.com/profile_images/1417752099488636931/cs2R59eW_400x400.jpg"
+          src="/images/logos/logo.png"
           alt="avatar"
           height="100"
           width="100"
@@ -202,7 +202,7 @@ const SkeletonFour = () => {
       </motion.div>
       <motion.div className="h-full relative z-20 w-1/3 rounded-2xl bg-white p-4 dark:bg-black dark:border-white/[0.1] border border-neutral-200 flex flex-col items-center justify-center">
         <Image
-          src="https://pbs.twimg.com/profile_images/1417752099488636931/cs2R59eW_400x400.jpg"
+          src="/images/logos/logo.png"
           alt="avatar"
           height="100"
           width="100"
@@ -220,7 +220,7 @@ const SkeletonFour = () => {
         className="h-full w-1/3 rounded-2xl bg-white p-4 dark:bg-black dark:border-white/[0.1] border border-neutral-200 flex flex-col items-center justify-center"
       >
         <Image
-          src="https://pbs.twimg.com/profile_images/1417752099488636931/cs2R59eW_400x400.jpg"
+          src="/images/logos/logo.png"
           alt="avatar"
           height="100"
           width="100"
@@ -273,7 +273,7 @@ const SkeletonFive = () => {
         className="flex flex-row rounded-2xl border border-neutral-100 dark:border-white/[0.2] p-2  items-start space-x-2 bg-white dark:bg-black"
       >
         <Image
-          src="https://pbs.twimg.com/profile_images/1417752099488636931/cs2R59eW_400x400.jpg"
+          src="/images/logos/logo.png"
           alt="avatar"
           height="100"
           width="100"
