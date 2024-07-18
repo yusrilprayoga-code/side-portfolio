b0260ff36d46890d7fca08bca2637e3ea9175f6f
yusrilprayoga-code <124210006@student.upnyk.ac.id>
2024-07-18 20:35:08 +0700

    Update Products description

--- src/constants/products.tsx
+++ src/constants/products.tsx
@@ -14,20 +14,25 @@ export const products = [
     content: (
       <div>
         <p>
-          Sit eiusmod ex mollit sit quis ad deserunt. Sint aliqua aliqua ullamco
-          dolore nulla amet tempor sunt est ipsum. Dolor laborum eiusmod
-          cupidatat consectetur velit ipsum. Deserunt nisi in culpa laboris
-          cupidatat elit velit aute mollit nisi. Officia ad exercitation laboris
-          non cupidatat duis esse velit ut culpa et.{" "}
+          B-Otomotif is a cutting-edge website that complements its popular
+          YouTube channel, focusing on the latest developments in the automotive
+          world. The site is dedicated to car enthusiasts, offering
+          comprehensive and in-depth content about new car models, detailed
+          reviews, and insightful discussions. Visitors can explore a variety of
+          articles, videos, and features that provide a closer look at the
+          newest vehicles on the market, making B-Otomotif a go-to resource for
+          anyone passionate about cars.{" "}
         </p>
         <p>
-          Exercitation pariatur enim occaecat adipisicing nostrud adipisicing
-          Lorem tempor ullamco exercitation quis et dolor sint. Adipisicing sunt
-          sit aute fugiat incididunt nostrud consequat proident fugiat id.
-          Officia aliquip laborum labore eu culpa dolor reprehenderit eu ex enim
-          reprehenderit. Cillum Lorem veniam eu magna exercitation.
-          Reprehenderit adipisicing minim et officia enim et veniam Lorem
-          excepteur velit adipisicing et Lorem magna.
+          The website&atos;s sleek and user-friendly design ensures an engaging
+          experience, allowing users to easily navigate through the wealth of
+          information available. With a strong emphasis on high-quality content,
+          B-Otomotif not only highlights the technical specifications and
+          performance of new cars but also delves into their design, innovation,
+          and market impact. This holistic approach ensures that readers and
+          viewers are well-informed, making B-Otomotif an essential destination
+          for staying up-to-date with the latest trends and advancements in the
+          automotive industry.
         </p>{" "}
       </div>
     ),
@@ -71,10 +76,7 @@ export const products = [
     description:
       "Nike is a design and development studio that focuses on building quality apps.",
     thumbnail: "/images/sidefolio-nike.png",
-    images: [
-      "/images/sidefolio-nike.png",
-      "/images/sidefolio-nike-2.png",
-    ],
+    images: ["/images/sidefolio-nike.png", "/images/sidefolio-nike-2.png"],
     stack: ["Reactjs", "Tailwindcss"],
     slug: "nike",
     content: (
@@ -104,10 +106,7 @@ export const products = [
     description:
       "ArCipe is a design and development studio that focuses on building quality apps.",
     thumbnail: "/images/sidefolio-arcipe.png",
-    images: [
-      "/images/sidefolio-arcipe.png",
-      "/images/sidefolio-arcipe-2.png",
-    ],
+    images: ["/images/sidefolio-arcipe.png", "/images/sidefolio-arcipe-2.png"],
     stack: ["Reactjs", "Tailwindcss"],
     slug: "arcipe",
     content: (
