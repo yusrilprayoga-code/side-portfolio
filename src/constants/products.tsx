25e52b21eaccb195677fd0dbbee44f0d3981dc57
yusrilprayoga-code <124210006@student.upnyk.ac.id>
2024-07-18 20:44:18 +0700

    Update Products description

--- src/constants/products.tsx
+++ src/constants/products.tsx
@@ -24,7 +24,7 @@ export const products = [
           anyone passionate about cars.{" "}
         </p>
         <p>
-          The website&atos;s sleek and user-friendly design ensures an engaging
+          The website is sleek and user-friendly design ensures an engaging
           experience, allowing users to easily navigate through the wealth of
           information available. With a strong emphasis on high-quality content,
           B-Otomotif not only highlights the technical specifications and
@@ -52,20 +52,24 @@ export const products = [
     content: (
       <div>
         <p>
-          Sit eiusmod ex mollit sit quis ad deserunt. Sint aliqua aliqua ullamco
-          dolore nulla amet tempor sunt est ipsum. Dolor laborum eiusmod
-          cupidatat consectetur velit ipsum. Deserunt nisi in culpa laboris
-          cupidatat elit velit aute mollit nisi. Officia ad exercitation laboris
-          non cupidatat duis esse velit ut culpa et.{" "}
+          CarShowroom is an informative website designed to provide
+          comprehensive details about car rental services. Catering to a wide
+          audience, from travelers seeking convenient transportation options to
+          locals in need of temporary vehicle solutions, CarShowroom serves as a
+          one-stop resource for all things related to car rentals. The website
+          features a vast array of information on various rental car options,
+          pricing, and rental locations, helping users make informed decisions
+          that best suit their needs.{" "}
         </p>
         <p>
-          Exercitation pariatur enim occaecat adipisicing nostrud adipisicing
-          Lorem tempor ullamco exercitation quis et dolor sint. Adipisicing sunt
-          sit aute fugiat incididunt nostrud consequat proident fugiat id.
-          Officia aliquip laborum labore eu culpa dolor reprehenderit eu ex enim
-          reprehenderit. Cillum Lorem veniam eu magna exercitation.
-          Reprehenderit adipisicing minim et officia enim et veniam Lorem
-          excepteur velit adipisicing et Lorem magna.
+          With a focus on user experience, CarShowroom offers an intuitive
+          interface that makes it easy for visitors to browse through different
+          car models, compare rental rates, and read reviews from other
+          customers. The platform is committed to ensuring transparency and
+          convenience, allowing users to quickly find the most suitable rental
+          cars for their specific requirements. Whether planning a weekend
+          getaway or needing a reliable vehicle for daily errands, CarShowroom
+          is the ultimate guide for hassle-free car rentals.
         </p>{" "}
       </div>
     ),
