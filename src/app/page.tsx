5cdaa8fe6cbc049b48f552b5d6e503926d991ec2
yusrilprayoga-code <124210006@student.upnyk.ac.id>
2024-07-21 11:23:06 +0700

    Update Projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,30 +4,43 @@ import { Highlight } from "@/components/Highlight";
 import { Paragraph } from "@/components/Paragraph";
 import { Products } from "@/components/Products";
 import { TechStack } from "@/components/TechStack";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <Container>
-      <span className="text-4xl">👋</span>
-      <Heading className="font-black dark:text-gray-300">Hello there! I&apos;m Yusril Prayoga</Heading>
-      <Paragraph className="max-w-xl mt-4 dark:text-gray-300">
-        I&apos;m a Front End developer that loves{" "}
-        <Highlight className="dark:text-gray-800">building products</Highlight> and web apps that can impact
-        millions of lives
-      </Paragraph>
-      <Paragraph className="max-w-xl mt-4 dark:text-gray-300">
-        I&apos;m a Junior software engineer with{" "}
-        <Highlight className="dark:text-gray-800">2 years of experience</Highlight> building scalable web apps
-        that are performance optimized and good looking.
-      </Paragraph>
-      <Heading
-        as="h2"
-        className="font-black text-lg md:text-lg lg:text-lg mt-20 mb-4 dark:text-gray-300"
-      >
-        What I&apos;ve been working on
-      </Heading>
-      <Products />
-      <TechStack />
+      <div className="flex flex-col items-center text-center">
+        <Image
+          src="https://media.licdn.com/dms/image/D5603AQEwz5nmhy87-g/profile-displayphoto-shrink_200_200/0/1715179622045?e=1726704000&v=beta&t=lOsGxfCJDsK9DD4POC9qIK-ODKnVs-JcqzuCTdk86ac"
+          width={300}
+          height={300}
+          alt="Yusril Prayoga"
+          className="rounded-full mt-4"
+        />
+        <Heading className="font-black dark:text-gray-300 mt-4">
+          Hello there! I&apos;m Yusril Prayoga
+        </Heading>
+        <Paragraph className="max-w-xl mt-4 dark:text-gray-300">
+          I&apos;m a Front End developer that loves{" "}
+          <Highlight className="dark:text-gray-800">building products</Highlight> and web apps that can impact
+          millions of lives.
+        </Paragraph>
+        <Paragraph className="max-w-xl mt-4 dark:text-gray-300">
+          I&apos;m a Junior software engineer with{" "}
+          <Highlight className="dark:text-gray-800">2 years of experience</Highlight> building scalable web apps
+          that are performance optimized and good looking.
+        </Paragraph>
+      </div>
+      <div className="mt-20">
+        <Heading
+          as="h2"
+          className="font-black text-lg md:text-lg lg:text-lg mb-4 dark:text-gray-300"
+        >
+          What I&apos;ve been working on
+        </Heading>
+        <Products />
+        <TechStack />
+      </div>
     </Container>
   );
 }
