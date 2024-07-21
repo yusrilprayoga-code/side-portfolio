84f9891e1d47043c6d303600c192c92782542a62
yusrilprayoga-code <124210006@student.upnyk.ac.id>
2024-07-21 16:14:00 +0700

    Update Products description

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -8,48 +8,41 @@ export const TechStack = () => {
     {
       title: "Next.js",
       src: "/images/logos/next.png",
-
       className: "h-10 w-14",
     },
     {
       title: "AWS",
       src: "/images/logos/aws.webp",
-
       className: "h-10 w-10",
     },
     {
       title: "Figma",
       src: "/images/logos/figma.png",
-
       className: "h-10 w-8",
     },
     {
       title: "Framer Motion",
       src: "/images/logos/framer.webp",
-
       className: "h-10 w-10",
     },
     {
       title: "Node",
       src: "/images/logos/node.png",
-
       className: "h-10 w-12",
     },
     {
       title: "Tailwind",
       src: "/images/logos/tailwind.png",
-
       className: "h-10 w-24",
     },
     {
       title: "Vercel",
       src: "/images/logos/vercel.png",
-
       className: "h-10 w-24",
     },
   ];
   return (
-    <div>
+    <div className="dark:text-gray-300 p-4 rounded-lg">
       <Heading
         as="h2"
         className="font-black text-lg md:text-lg lg:text-lg mt-20 mb-4"
