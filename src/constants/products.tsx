b4d81dc1bdc52bf5084918f6992a1ad76d6c9eff
yusrilprayoga-code <124210006@student.upnyk.ac.id>
2024-08-29 13:47:17 +0700

    Update projects

--- src/constants/products.tsx
+++ src/constants/products.tsx
@@ -294,7 +294,7 @@ export const products = [
     ),
   },
   {
-    href: "https://github.com/yusrilprayoga-code/ecommerce-app.git",
+    href: "https://github.com/yusrilprayoga-code/upload-image-with-vercel.git",
     title: "PicStash",
     description:
       "A design and development studio that focuses on building quality apps.",
