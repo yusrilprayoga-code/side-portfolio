568329e91cc8f2c2144d2214dfa7bda01cc4bd2a
yusrilprayoga-code <124210006@student.upnyk.ac.id>
2025-01-20 23:12:18 +0700

    Feat: Add Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { twMerge } from "tailwind-merge";
 import { Footer } from "@/components/Footer";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({
   subsets: ["latin"],
