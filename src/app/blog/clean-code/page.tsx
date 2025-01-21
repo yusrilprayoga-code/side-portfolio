57541c1d977d76f62d926164efd073329f7b382c
yusrilprayoga-code <124210006@student.upnyk.ac.id>
2025-01-21 09:39:39 +0700

    Feat: Update Metadata for SEO

--- src/app/blog/clean-code/page.tsx
+++ src/app/blog/clean-code/page.tsx
@@ -1,5 +1,21 @@
 import Content from "./content.mdx";
+import { BlogLayout } from "@/components/BlogLayout";
+
+export const meta = {
+  date: "2024-07-17",
+  title: "Writing Clean Code With React",
+  description:
+    "Effective and efficient ways to write clean code with React while keeping in mind the performance and maintainability of the codebase.",
+  image:
+    "https://images.unsplash.com/photo-1542831371-29b0f74f9713?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  tags: ["Clean Code"],
+};
 
 export default function Page() {
-  return <Content />;
+  return (
+    <BlogLayout meta={meta}>
+      <Content />
+    </BlogLayout>
+  );
+
 }
--- src/app/blog/tailwindcss-tips-and-tricks/page.tsx
+++ src/app/blog/tailwindcss-tips-and-tricks/page.tsx
@@ -1,5 +1,20 @@
 import Content from "./content.mdx";
+import { BlogLayout } from "@/components/BlogLayout";
+
+export const meta = {
+  date: "2024-07-17",
+  title: "Tailwindcss tips and tricks to conquer the world",
+  description:
+    "Tips and Tricks Tailwdind Css",
+  image:
+    "https://images.unsplash.com/photo-1555066931-4365d14bab8c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2370&q=80",
+  tags: ["tailwindcss", "css", "frontend"],
+};
 
 export default function Page() {
-  return <Content />;
+  return (
+    <BlogLayout meta={meta}>
+      <Content />
+    </BlogLayout>
+  );
 }
