1883911f2be278acdbc8203b05db75b8dd2fba23
yusrilprayoga-code <124210006@student.upnyk.ac.id>
2025-01-21 09:28:01 +0700

    Feat: Update Metadata for SEO

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -12,30 +12,6 @@ export const metadata: Metadata = {
   title: "Blogs | Yusril Prayoga",
   description:
     "Yusril Prayoga is a developer, writer and speaker. He is a digital nomad and travels around the world while working remotely.",
-  keywords: [
-    "developer",
-    "writer",
-    "speaker",
-    "digital",
-    "remote work",
-    "yusrilprayoga",
-    "yusrilprayoga-code",
-    "portfolio",
-    "yusril prayoga",
-    "yusrilprayoga code",
-    "yusrilprayoga portfolio",
-    "yusril prayoga portfolio",
-    "yusrilprayoga-code portfolio",
-    "yusrilprayoga-code portfolio",
-    "yusrilprayoga code portfolio",
-    "yusril prayoga tech",
-    "yusrilprayoga tech",
-    "yusrilprayoga-code tech",
-    "yusrilprayoga code tech",
-    "yusrilprayoga tech portfolio",
-    "yusrilprayoga-code tech portfolio",
-    "yusrilprayoga-code tech portfolio",
-  ],
 };
 
 export default async function Blog() {
