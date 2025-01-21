07af1976c48ba83b8edf7f39018c68d6829d04d6
yusrilprayoga-code <124210006@student.upnyk.ac.id>
2025-01-21 09:31:05 +0700

    Feat: Update Metadata for SEO

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -22,9 +22,9 @@ export const Badge = ({ text, href }: { text: string; href: string }) => {
         >
           <motion.path
             stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="1.5"
             d="M10.75 8.75L14.25 12L10.75 15.25"
             initial={{ pathLength: 0 }}
             animate={{ pathLength: 1 }}
